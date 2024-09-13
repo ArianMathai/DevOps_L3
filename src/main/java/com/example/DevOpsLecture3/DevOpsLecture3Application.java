0482dc489fbebc7168e1f409b280605dd4363eef
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2024-09-13 09:35:18 +0200

    git init

---
+++ src/main/java/com/example/DevOpsLecture3/DevOpsLecture3Application.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsLecture3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsLecture3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsLecture3Application.class, args);
+	}
+
+}
